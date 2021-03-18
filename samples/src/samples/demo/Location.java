4422062e105d81a51d077938b7a5b542108c9312
carol-lim <b032020025@student.utem.edu.my>
2021-03-18 22:35:58 +0800

    Location.java - initial commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,15 @@
+package samples.demo;
+
+public class Location {
+	private String myHome;
+
+	//Getter
+	public String getMyHome() {
+		return myHome;
+	}
+	
+	//Setter
+	public void setHappiness(String newMyHome){
+		this.myHome = newMyHome;
+	}
+}
