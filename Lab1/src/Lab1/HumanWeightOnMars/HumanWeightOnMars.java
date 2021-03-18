9d8a9a533d0b421d7b062d065d8ac7cd5f688172
carol-lim <b032020025@student.utem.edu.my>
2021-03-19 00:37:49 +0800

    proper oop

--- Lab1/src/Lab1/HumanWeightOnMars/HumanWeightOnMars.java
+++ Lab1/src/Lab1/HumanWeightOnMars/HumanWeightOnMars.java
@@ -1,17 +1,31 @@
 package Lab1.HumanWeightOnMars;
 
 import java.text.DecimalFormat;
+import java.util.Scanner;
 public class HumanWeightOnMars {
 	private static DecimalFormat df = new DecimalFormat("0.00");
+	private double weightOnEarth;
+	
+	public void setWeightOnEarth(double woe) {
+		this.weightOnEarth = woe;
+	}
+	
+	public double getWeightOnEarth() {
+		return weightOnEarth;
+	}
 
-	public static double calculate(double kg) {
-		double mars = kg/9.81*3.711;
-		return mars;
+	public static double calculate(double weightOnEarth) {
+		return weightOnEarth/9.81*3.711;
 	}
 	
 	public static void main(String args[]) {
-		double weigth = 58;
-		System.out.println(weigth+"kg on Earth = "+df.format(calculate(weigth))+"kg on Mars");
+		Scanner sr = new Scanner(System.in);
+		System.out.println("Enter your weight on Earth (kg): ");
+		double woe = sr.nextDouble();
+		HumanWeightOnMars human1 = new HumanWeightOnMars();
+		human1.weightOnEarth = woe;
+		System.out.println(human1.weightOnEarth+"kg on Earth = "+df.format(calculate(human1.weightOnEarth))+"kg on Mars");
 	}
 	
 }
+
