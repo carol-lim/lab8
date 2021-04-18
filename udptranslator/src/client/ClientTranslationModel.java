6ebc64f84c9d559b59f42d1634a6002a0d672b71
carol-lim <b032020025@student.utem.edu.my>
2021-04-18 23:18:07 +0800

    initial commit

---
+++ udptranslator/src/client/ClientTranslationModel.java
@@ -0,0 +1,6 @@
+package client;
+
+public class ClientTranslationModel {
+	protected String[] english = {"Good morning", "Good night", "How are you?", "Thank you", "Goodbye", "What’s up?"};
+	protected String[] languages = {"Malay", "Arabic", "Korean"};
+}
