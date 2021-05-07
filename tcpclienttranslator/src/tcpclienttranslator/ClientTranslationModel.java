d933e6439b317a2eb8c11f71b68a617f8fc3f3a6
carol-lim <80899293+carol-lim@users.noreply.github.com>
2021-05-07 12:29:55 +0800

    Delete tcpclienttranslator directory

--- tcpclienttranslator/src/tcpclienttranslator/ClientTranslationModel.java
+++
@@ -1,6 +0,0 @@
-package tcpclienttranslator;
-
-public class ClientTranslationModel {
-	protected String[] english = {"Good morning", "Good night", "How are you?", "Thank you", "Goodbye", "What’s up?"};
-	protected String[] languages = {"Malay", "Arabic", "Korean"};
-}
