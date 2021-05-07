9c7550641402d8a3ea7ccade4cf91cf88ce44c72
carol-lim <80899293+carol-lim@users.noreply.github.com>
2021-05-07 12:30:31 +0800

    Delete udptranslator directory

--- udptranslator/src/client/ClientTranslationModel.java
+++
@@ -1,6 +0,0 @@
-package client;
-
-public class ClientTranslationModel {
-	protected String[] english = {"Good morning", "Good night", "How are you?", "Thank you", "Goodbye", "What’s up?"};
-	protected String[] languages = {"Malay", "Arabic", "Korean"};
-}
